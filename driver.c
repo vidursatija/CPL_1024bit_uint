e61beec630214da8cda6a6f8cb0f2dfb66ccd4a7
vidursatija <vidursatija@gmail.com>
2018-02-11 21:06:37 +0530

    Add docs in .h files, changed driver

--- driver.c
+++ driver.c
@@ -3,17 +3,20 @@
 
 int main()
 {
-	char c[80] = "ff\0";
-	char d[80] = "100000000\0";
+	char c[257];// = "ff\0";
+	char d[257];// = "100000000\0";
+	printf("Enter 2 numbers in hex\n");
+	scanf("%s%s", c, d)
 	uint1024_t a = hex_to_1024(c);
 	uint1024_t b = hex_to_1024(d);
-	//printInts(a);
-	//printInts(b);
+
 	uint1024_t add = uint1024_add(a, b);
-	//printInts(add);
-	char *res = u1024_to_hex(add);
-	printf("%s\n", res);
+	uint1024_t sub = uint1024_sub(a, b);
 	uint1024_t mul = uint1024_mul(a, b);
-	res = u1024_to_hex(mul);
-	printf("%s\n", res);
+
+	printf("Addition sum: %s\n", u1024_to_hex(add));
+	printf("Subtraction sum: %s\n", u1024_to_hex(sub));
+	printf("Multiplication sum: %s\n", u1024_to_hex(mul));
+
+	return 0;
 }
--- uint1024_t.h
+++ uint1024_t.h
@@ -10,22 +10,45 @@ typedef struct
 	uint32_t *ints;
 }uint1024_t;
 
+/* void printInts(uint1024_t)
+ * takes an unsigned 1024 bit number and prints all 32 integers
+ */
 void printInts(uint1024_t a);
 
+/* uint1024_t hex_to_1024(char*)
+ * takes the hexadecimal string and converts it to 1024 bit number
+ */
 uint1024_t hex_to_1024(char *hex);
 
+/* char* u1024_to_hex(uint1024_t)
+ * takes the 1024 bit number and converts it to a hex number
+ */
 char* u1024_to_hex(uint1024_t temp);
 
+/* uint1024_t uint_to_1024(unsigned int)
+ * takes an unsigned integer and converts it to a 1024 bit number
+ */
 uint1024_t uint_to_1024(unsigned int i);
 
+/* 1024 bit 0 constant function */
 uint1024_t uint1024_0();
 
+/* 1024 bit 1 constant function */
 uint1024_t uint1024_1();
 
+/* uint1024_t uint1024_add(uint1024, uint1024)
+ * takes 2 1024 bit numbers and adds them up. Returns the result without carry
+ */
 uint1024_t uint1024_add(uint1024_t a, uint1024_t b);
 
+/* uint1024_t uint1024_sub(uint1024, uint1024)
+ * takes 2 1024 bit numbers and subtracts second operand from first. Returns the result without carry
+ */
 uint1024_t uint1024_sub(uint1024_t a, uint1024_t b);
 
+/* uint1024_t uint1024_add(uint1024, uint1024)
+ * takes 2 1024 bit numbers and multiplies them. Returns the result without carry
+ */
 uint1024_t uint1024_mul(uint1024_t a, uint1024_t b);
 
 #endif
